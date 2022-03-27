438d76ab64a0fda133a0a25ab01c56d541200422
khailee <fcxomben99@gmail.com>
2022-03-27 23:30:41 +0700

    add avatar for user

--- frontend/src/components/ecomm/Register.js
+++ frontend/src/components/ecomm/Register.js
@@ -4,7 +4,8 @@ import {useNavigate} from 'react-router-dom';
 function Register() {
   const [name, setName] = useState("");
   const [password, setPassowrd] = useState("");
-  const [email, seetEmail] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatar, setAvatar] = useState("");
   const nagative = useNavigate();
 
   useEffect(() => {
@@ -15,14 +16,17 @@ function Register() {
   });
   
   async function signUp() {
-    let item = {name, password, email};
+    let item = {name, password, email, avatar};
+    console.log(item);
+    const formdata = new FormData();
+    formdata.append('name', name);
+    formdata.append('avatar', avatar);
+    formdata.append('email', email);
+    formdata.append('password', password);
+
     let result = await fetch("http://techblog.backend.com:9982/api/v1/auth/register", {
       method: "POST",
-      body: JSON.stringify(item),
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": 'application/json'
-      }
+      body: formdata
     });
     result = await result.json();
     if(result.user) {
@@ -35,11 +39,13 @@ function Register() {
   return (
     <div className="col-sm-6 offset-sm-3">
       <h1>Register page</h1>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" placeholder="name" />
+      <input type="text" onChange={(e) => setName(e.target.value)} className="form-control" placeholder="name" />
       <br />
-      <input type="email" value={email} onChange={(e) => seetEmail(e.target.value)} className="form-control" placeholder="email" />
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="email" />
       <br />
-      <input type="password" value={password} onChange={(e) => setPassowrd(e.target.value)} className="form-control" placeholder="password" />
+      <input type="file" onChange={(e) => setAvatar(e.target.files[0])} className="form-control" placeholder="email" />
+      <br />
+      <input type="password" onChange={(e) => setPassowrd(e.target.value)} className="form-control" placeholder="password" />
       <br />
       <button onClick={signUp} className="btn btn-primary">Sign up</button>
     </div>
