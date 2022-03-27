0a5e8d64274d9d30e09976725d7f983d54127b06
khailee <fcxomben99@gmail.com>
2022-03-27 15:55:55 +0700

    react logout

--- frontend/src/components/ecomm/Header.js
+++ frontend/src/components/ecomm/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const info = JSON.parse(localStorage.getItem('user-info'));
+  const nagative = useNavigate();
+
+  function logout() {
+    localStorage.clear();
+    nagative('/login');
+  }
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -13,6 +22,12 @@ function Header() {
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
         </Nav>
+        {info ? 
+        <Nav>
+          <NavDropdown title={info && info.user.name}>
+            <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+          </NavDropdown>
+        </Nav> : ''}
       </Container>
     </Navbar>
   )
